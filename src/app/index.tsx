d5e60d9d1fb701187cd78de7eea5934c676e639a
Sudip Shrestha <sudipstha08@gmail.com>
2021-05-16 22:22:20 +0545

    add: ➕ nprogress bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,13 +9,17 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { GlobalStyle } from 'styles/global-styles';
+import { useTranslation } from 'react-i18next';
 
 import { HomePage } from './pages/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
+import { TopProgressBar } from './components';
 import { BooksPage } from './pages/BooksPage';
-import { useTranslation } from 'react-i18next';
+
+// Create a client
+const queryClient = new QueryClient();
 
 export function App() {
   const { i18n } = useTranslation();
@@ -27,12 +31,17 @@ export function App() {
         htmlAttributes={{ lang: i18n.language }}
       >
         <meta name="description" content="A React Boilerplate application" />
+        <link rel="stylesheet" href="nprogress.css" />
       </Helmet>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/books" component={BooksPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <TopProgressBar />
+      {/* Provide the client to your App */}
+      <QueryClientProvider client={queryClient}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/books" component={BooksPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </QueryClientProvider>
       <GlobalStyle />
     </BrowserRouter>
   );
