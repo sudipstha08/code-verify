1d03a89b7d694cd1ff81812598aaf973da1c9461
sudipstha08 <sudipstha08@gmail.com>
2024-06-20 21:23:53 +0545

    feat: add verification component event handler

--- client/src/components/organisms/Verification/index.tsx
+++ client/src/components/organisms/Verification/index.tsx
@@ -1,12 +1,15 @@
-import { ChangeEvent, FC, useState } from 'react'
+import { ChangeEvent, FC, useRef, useState } from 'react'
 import styled from 'styled-components'
+import { INPUT_LENGTH, KEYBPARD_KEYS } from '@/constants'
 
 const Container = styled.main``
 
 export const Verification: FC = () => {
   const [values, setValues] = useState<string[]>(Array(6).fill(''))
+  const itemsRef = useRef<Map<number, HTMLInputElement>>(new Map())
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (values.filter(a => a).length === INPUT_LENGTH || !e.target.value) return
     const idx = parseInt(e.target.dataset.id as string)
 
     setValues(prevValue => {
@@ -14,6 +17,37 @@ export const Verification: FC = () => {
       value[idx] = e.target.value
       return value
     })
+    scrollToNextInput(idx + 1)
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (!(e.target instanceof HTMLInputElement)) {
+      return
+    }
+    const idx = parseInt(e?.target?.dataset.id as string)
+    if (e.key === KEYBPARD_KEYS.BACKSPACE) {
+      scrollToNextInput(idx - 1)
+      if (!values[idx]) return
+      setValues(prevValue => {
+        const value: string[] = Object.assign([], prevValue)
+        value[idx] = ''
+        return value
+      })
+    }
+  }
+
+  function scrollToNextInput(currentIdx: number) {
+    if (currentIdx < 0) return
+    const map = getMap()
+    const node = map.get(currentIdx)
+    node?.focus()
+  }
+
+  function getMap() {
+    if (!itemsRef.current) {
+      itemsRef.current = new Map()
+    }
+    return itemsRef.current
   }
 
   return (
@@ -24,7 +58,16 @@ export const Verification: FC = () => {
             value={value}
             onChange={handleChange}
             data-id={idx}
+            onKeyDown={handleKeyDown}
             key={idx}
+            ref={node => {
+              const map = getMap()
+              if (node) {
+                map.set(idx, node)
+              } else {
+                map.delete(idx)
+              }
+            }}
           />
         )
       })}
---
+++ client/src/constants/keyboard.ts
@@ -0,0 +1,5 @@
+export const KEYBPARD_KEYS = {
+  BACKSPACE: 'Backspace',
+}
+
+export const INPUT_LENGTH = 6
